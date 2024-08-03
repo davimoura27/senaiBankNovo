134936c51614152af6974a45e1bf4a01dc1b4f5c
Davi moura <davimoura27@gmail.com>
2024-08-02 21:27:07 -0300

    senaiBank

--- senaibank/src/main/java/com/api/senaibank/classe/repository/ClienteRepository.java
+++ senaibank/src/main/java/com/api/senaibank/classe/repository/ClienteRepository.java
@@ -1,7 +1,12 @@
 package com.api.senaibank.classe.repository;
+
 import com.api.senaibank.classe.Cliente;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    List<Cliente> findByAtivoTrue();
 }
